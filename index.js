c85d2ba64ab5b8ce8cc4c832f763ec0fedbe2c5a
Fabiola De La Torre <cubias.fabiola@gmail.com>
2023-05-19 03:21:33 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+let totalBatteries = batteryBatches.reduce(function(accum, batteries) {
+    return batteries + accum;
+}, 0);
 // Code your solution here
